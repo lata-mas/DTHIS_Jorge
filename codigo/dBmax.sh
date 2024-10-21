2dbaf3dfff11f6c1f3a1a767e6f1ce54982a587b
Jorge Luis <106550316+kindayourdude@users.noreply.github.com>
2024-10-21 14:09:39 -0600

    Update dBmax.sh

--- codigo/dBmax.sh
+++ codigo/dBmax.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-chale="/home/pi/DTHIS_jorge/dBmax.txt"
+maximo="/home/pi/DTHIS_jorge/dBmax.txt"
 
-/usr/bin/sox /home/pi/DTHIS_jorge/audio.wav -n stat 2>&1 | /usr/bin/grep "Maximum amplitude:" | /usr/bin/awk '{print $3}'  > $chale
+/usr/bin/sox /home/pi/DTHIS_jorge/audio.wav -n stat 2>&1 | /usr/bin/grep "Maximum amplitude:" | /usr/bin/awk '{print $3}'  > $maximo
