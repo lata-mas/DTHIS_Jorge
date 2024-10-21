e0a145236a35d465c1429f4c97f19e18297b554e
Jorge Luis <106550316+kindayourdude@users.noreply.github.com>
2024-10-21 14:18:27 -0600

    Update rms.sh

--- codigo/rms.sh
+++ codigo/rms.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-puesya="/home/pi/DTHIS_jorge/rms.txt"
+media="/home/pi/DTHIS/rms.txt"
 
-/usr/bin/sox /home/pi/DTHIS_jorge/audio.wav -n stat 2>&1 | /usr/bin/grep "RMS     amplitude:" | /usr/bin/awk '{print $3}' > $puesya
+/usr/bin/sox /home/pi/DTHIS/audio.wav -n stat 2>&1 | /usr/bin/grep "RMS     amplitude:" | /usr/bin/awk '{print $3}' > $media
