9e2921d11eb4074ae22ff5b8b7a1fba5a42b3d03
Jorge Luis <106550316+kindayourdude@users.noreply.github.com>
2024-10-21 13:15:50 -0600

    Update grabar.sh

--- codigo/grabar.sh
+++ codigo/grabar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-muestra="/home/pi/DTHIS_jorge/audio.wav"
+grabar="/home/pi/DTHIS_jorge/audio.wav"
 
-arecord -D plughw:2,0 -f cd -t wav -d 15 -r 44100 $muestra
+arecord -D plughw:2,0 -f cd -t wav -d 15 -r 44100 $grabar
