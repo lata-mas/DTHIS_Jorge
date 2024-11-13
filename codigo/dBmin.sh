a807c1db30c8784a521f47d26e500a3cfe555385
Jorge Luis <106550316+kindayourdude@users.noreply.github.com>
2024-11-13 09:48:35 -0600

    Update dBmin.sh

--- codigo/dBmin.sh
+++ codigo/dBmin.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-minimo="/home/pi/DTHIS/dBmin.txt"
+minimo="/home/pi/DTHIS/txt/dBmin.txt"
 
 /usr/bin/sox /home/pi/DTHIS/audio.wav -n stat 2>&1 | /usr/bin/grep "Minimum amplitude:" | /usr/bin/awk '{print $3}' > $minimo
